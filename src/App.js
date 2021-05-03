b7cf85ebdfb8fb0e758de9dc5f757b6c21bc7c14
krarm2209 <krishna.aroomoogon@mail.mcgill.ca>
2021-05-03 15:00:31 +0400

    react setup

--- src/App.js
+++ src/App.js
@@ -1,14 +1,16 @@
 import logo from './logo.svg';
 import './App.css';
 
+import Posts from './components/Posts';
+import Postform from './components/Postform';
+
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
+        <Postform />
+        <Posts />
         <a
           className="App-link"
           href="https://reactjs.org"
